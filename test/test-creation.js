c5512555a97f40521f30d9d4143096628231df0c
Peter Vilja <JP@Marinella-MacBook-Air.local>
2013-10-28 21:40:38 +0200

    Added a new test case

--- test/test-creation.js
+++ test/test-creation.js
@@ -9,7 +9,9 @@ var expect = chai.expect;
 
 
 describe('scalatra generator', function () {
-  before(function (done) {
+  before(createGenerator);
+
+  function createGenerator(done) {
     helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
       if (err) {
         return done(err);
@@ -20,9 +22,65 @@ describe('scalatra generator', function () {
       ]);
       done();
     }.bind(this));
-  });
+  }
 
   it('creates expected files', function (done) {
+    var expected = [
+      // add files you expect to exist here.
+      'sbt',
+      'project/plugins.sbt',
+      'project/build.properties',
+      'project/build.scala',
+      'src/main/scala/ScalatraBootstrap.scala',
+      'src/main/scala/com/peter/app/MessageServlet.scala',
+      'src/main/webapp/WEB-INF/web.xml'
+    ];
+
+    helpers.mockPrompt(this.app, {
+      'projectName': 'scalatra-app',
+      'organization': 'com.peter',
+      'package': 'app',
+      'servletName': 'MessageServlet'
+    });
+    this.app.options['skip-install'] = true;
+    this.app.run({}, function () {
+      helpers.assertFiles(expected);
+      done();
+    });
+  });
+
+  it('writes correct values', function (done) {
+    var build = this.app.readFileAsString('project/build.scala');
+    build.should.be.a('string');
+    expect(build).to.contain('val Organization = "com.peter"');
+    expect(build).to.contain('val Name = "scalatra-app"');
+
+    var bootstrap = this.app.readFileAsString('src/main/scala/ScalatraBootstrap.scala');
+    bootstrap.should.be.a('string');
+    expect(bootstrap).to.contain('context.mount(new MessageServlet, "/")');
+    expect(bootstrap).to.contain('import com.peter.app._');
+
+    testFilesUnderPackageStructure(this.app);
+    
+    done();
+  });
+
+  function testFilesUnderPackageStructure(app) {
+    var servlet = app.readFileAsString('src/main/scala/com/peter/app/MessageServlet.scala');
+    servlet.should.be.a('string');
+    expect(servlet).to.contain('package com.peter.app');
+    expect(servlet).to.contain('class MessageServlet');
+
+    var spec = app.readFileAsString('src/test/scala/com/peter/app/MessageServletSpec.scala');
+    spec.should.be.a('string');
+    expect(spec).to.contain('package com.peter.app');
+    expect(spec).to.contain('class MessageServletSpec');
+    expect(spec).to.contain('addServlet(classOf[MessageServlet], "/*")')
+  }
+
+  before(createGenerator);
+
+  it("creates correct package structure even when instructions are misundrestood", function (done) {
     var expected = [
       // add files you expect to exist here.
       'sbt',
@@ -43,31 +101,9 @@ describe('scalatra generator', function () {
     this.app.options['skip-install'] = true;
     this.app.run({}, function () {
       helpers.assertFiles(expected);
-      done();
     });
-  });
-
-  it('writes correct values', function (done) {
-    var build = this.app.readFileAsString('project/build.scala');
-    build.should.be.a('string');
-    expect(build).to.contain('val Organization = "com.peter"');
-    expect(build).to.contain('val Name = "scalatra-app"');
-
-    var bootstrap = this.app.readFileAsString('src/main/scala/ScalatraBootstrap.scala');
-    bootstrap.should.be.a('string');
-    expect(bootstrap).to.contain('context.mount(new MessageServlet, "/")');
-    expect(bootstrap).to.contain('import com.peter.app._');
     
-    var servlet = this.app.readFileAsString('src/main/scala/com/peter/app/MessageServlet.scala');
-    servlet.should.be.a('string');
-    expect(servlet).to.contain('package com.peter.app');
-    expect(servlet).to.contain('class MessageServlet');
-
-    var spec = this.app.readFileAsString('src/test/scala/com/peter/app/MessageServletSpec.scala');
-    spec.should.be.a('string');
-    expect(spec).to.contain('package com.peter.app');
-    expect(spec).to.contain('class MessageServletSpec');
-    expect(spec).to.contain('addServlet(classOf[MessageServlet], "/*")')
+    testFilesUnderPackageStructure(this.app);
     done();
-  });
+  });  
 });
